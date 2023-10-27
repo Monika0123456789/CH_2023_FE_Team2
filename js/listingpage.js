8b499d4006bd56011b6f18ed26de41ba46ce1b3f
Suhas Indrigi <suhaspoo24@gmail.com>
2023-10-27 17:38:25 +0530

    comments included for page1 and page2 for hotel-listing-page, page 2 implemented for hotel-listing-page included fetching json data, toggle functionality included for hotel-listing-page.

--- js/listingpage.js
+++ js/listingpage.js
@@ -1,32 +1,31 @@
-// function to change the page upon button click
-function changePage(event){
-    // if page2 container is hidden and page1 container is not hidden
-    if( !($("#page1-container").is(":hidden")) && $("#page2-container").is(":hidden") ){
-        // hide page1 and display page2
-        $("#page1-container").hide();
-        $("#page2-container").show();
+// function to switch to page2
+function changePageToTwo(){
+    // hide page1 and show page2 as well as change the color of button2 to active
+    $("#page1-container").hide();
+    $("#page2-container").show();
 
-        //changing the styles of buttons
-        $("#toggle-page2-button2").css({"background-color":"blue","color":"white"});
-
-    }
-    // if page1 container is hidden and page2 container is not hidden
-    else if( !($("#page2-container").is(":hidden")) && $("#page1-container").is(":hidden") ){
-        // hide page2 and display page1
-        $("#page2-container").hide();
-        $("#page1-container").show();
-
-        //changing the styles of buttons
-        $("#toggle-page1-button1").css({"background-color":"blue","color":"white"});
-    }
+    $("#toggle-button1").css({"background-color":"rgb(216, 204, 189)", "color":"black"});
+    $("#toggle-button2").css({"background-color":"blue", "color":"white"});
 }
 
+// function to switch to page1
+function changePageToOne(){
+    // hide page2 and show page1 as well as change the color of button1 to active
+    $("#page2-container").hide();
+    $("#page1-container").show();
+
+    $("#toggle-button2").css({"background-color":"rgb(216, 204, 189)", "color":"black"});
+    $("#toggle-button1").css({"background-color":"blue", "color":"white"});
+}
 
 $(() => {
+    // // hide page2 on page load
     $("#page2-container").hide();
-    $("#toggle-page1-button1").css({"background-color":"blue","color":"white"})
+    $("#toggle-button1").css({"background-color":"blue","color":"white"})
     
-    // invoke function changePage upon clicking toggle buttons
-    $(".toggle-buttons").on("click",changePage);
+    // invoke function changePagetoTwo upon clicking toggle button 2
+    $("#toggle-button2").on("click",changePageToTwo);
 
+    // invoke function changePagetoOne upon clicking toggle button 1
+    $("#toggle-button1").on("click",changePageToOne);
 })
