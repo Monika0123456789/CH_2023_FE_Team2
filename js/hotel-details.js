6dda6d24af85e23fd201afc1d1c8c27086502a6f
Suhas Indrigi <suhaspoo24@gmail.com>
2023-10-31 14:54:49 +0530

    location logo in the hotel-details page changed. changing the image in the section1 of hotel-details page. adding margin-top to the section2 in hotel-details page. changing the background color of icons in the section2 of hotel-details page. changing the icons in the section2 of the hotel-details page. changing the icons in the amenities section. changing the script functionality in the script. hotel details pop up feature improvised.

--- js/hotel-details.js
+++ js/hotel-details.js
@@ -11,51 +11,49 @@ function init() {
   document.getElementById("closeMap").addEventListener("click", closeHandler);
 
 // to load the room details when a user selects the room
-  document.getElementById("room-popup").addEventListener("click", function () {
+  document.getElementsByClassName("hotel-detail-maincontainer")[0].addEventListener("click", function () {
     $("#popup-container").html(`
+        <section class="card">
+        <div class="popup-head">
+        <h3>Room Detail</h3>
+        <h3  id="close-popup">&times;</h3>
+        </div>
 
-
-<section class="card">
-<div class="popup-head">
-<h3>Room Detail</h3>
-<h3  id="close-popup">X</h3>
-</div>
-
-<hr>
-<img src="./assets/images/12(1).jpg" alt="Room Image">
-<div class="room-details">
-<h1>Deluxe Pool View</h1>
-<p>Club rooms are well furnished with air conditioner,32 inch LCD television and a mini bar.
-    They have attached bathroom with shower head and hair dryer and 24 hours supply of hot water.
-    Complimentary wireless internet access is available. Additional amenities include bottled water, a
-    safe
-    and a desk.
-</p>
-<h3>
-    Amenities
-</h3>
-<table>
-    <tbody>
-        <tr>
-            <td><img src="./assets/images/tick.svg" alt="tick"> Swiming Pool</td>
-            <td><img src="./assets/images/tick.svg" alt="tick"> TV</td>
-        </tr>
-        <tr>
-            <td><img src="./assets/images/tick.svg" alt="tick"> Spa</td>
-            <td><img src="./assets/images/tick.svg" alt="tick"> Mirror</td>
-        </tr>
-        <tr>
-            <td><img src="./assets/images/tick.svg" alt="tick"> Kids Play Are</td>
-            <td><img src="./assets/images/tick.svg" alt="tick"> AC</td>
-        </tr>
-        <tr>
-            <td><img src="./assets/images/tick.svg" alt="tick"> Gym</td>
-            <td><img src="./assets/images/tick.svg" alt="tick"> Slippers</td>
-        </tr>
-    </tbody>
-</table>
-</div>
-</section>
+        <hr>
+        <img src="./assets/images/12(1).jpg" alt="Room Image">
+        <div class="room-details">
+        <h1>Deluxe Pool View</h1>
+        <p>Club rooms are well furnished with air conditioner,32 inch LCD television and a mini bar.
+            They have attached bathroom with shower head and hair dryer and 24 hours supply of hot water.
+            Complimentary wireless internet access is available. Additional amenities include bottled water, a
+            safe
+            and a desk.
+        </p>
+        <h3>
+            Amenities
+        </h3>
+        <table>
+            <tbody>
+                <tr>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> Swiming Pool</td>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> TV</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> Spa</td>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> Mirror</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> Kids Play Are</td>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> AC</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> Gym</td>
+                    <td><img src="./assets/images/tick.svg" alt="tick"> Slippers</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+        </section>
 
 `);
     document.getElementById("popup-container").style.display = "block";
