74712d8e4f6bace17e97f981c6feea4d1f3737d0
Monika Muniraju <Monika.Muniraju@valtech.com>
2023-11-21 17:45:49 +0530

    Delete hotels directory

--- hotels/src/webPages/landingPage/components/hotelLandingPageMain.jsx
+++
@@ -1,8 +0,0 @@
-// main component of this webpage -- here we will render the components of this page
-export let HotelLandingPageMain = () => {
-  return (
-    <div>
-      <h1>hello from hotel landing page</h1>
-    </div>
-  );
-};
