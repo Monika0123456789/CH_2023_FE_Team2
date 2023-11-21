e4ff1848f02d61f22f4a549a60e4197bc4ef2540
Jeevan <Jeevan15sj@gmail.com>
2023-11-21 17:12:09 +0530

    Section 1 Completed with the react.

---
+++ POC-2/hotels/src/webPages/landingPage/components/header.jsx
@@ -0,0 +1,16 @@
+import "../components/header.css";
+
+export let HeaderComp = () => {
+    return (
+        <header class="main-header">
+        <img class="headerimage logo" src="./assets/images/logo.svg" alt="logo" onclick="window.location.href='landingpage.html' "/>
+        <img class="headerimage profile" src="./assets/images/01.jpg" alt="profile"/>
+    </header>
+      
+    
+        
+        
+      
+    );
+  };
+
--- POC-2/hotels/src/webPages/landingPage/components/hotelLandingPageMain.jsx
+++ POC-2/hotels/src/webPages/landingPage/components/hotelLandingPageMain.jsx
@@ -1,8 +1,13 @@
+import { HeaderComp } from "./header";
+import { SectionOneComp } from "./section1";
+
 // main component of this webpage -- here we will render the components of this page
 export let HotelLandingPageMain = () => {
   return (
     <div>
-      <h1>hello from hotel landing page</h1>
+      <HeaderComp/>
+      <SectionOneComp/>
+      
     </div>
   );
 };
