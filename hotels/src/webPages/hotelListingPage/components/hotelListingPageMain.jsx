b6b31adf6fa097a77020815948cf474f9bfe1a1a
Mohammad Roohullah <Mohammad.Roohullah@valtech.com>
2023-11-21 16:09:00 +0530

    initial project structure

---
+++ hotels/src/webPages/hotelListingPage/components/hotelListingPageMain.jsx
@@ -0,0 +1,8 @@
+// main component of this webpage -- here we will render the components of this page
+export let HotelListingPageMain = () => {
+  return (
+    <div>
+      <h1>hello from hotel listing page</h1>
+    </div>
+  );
+};
