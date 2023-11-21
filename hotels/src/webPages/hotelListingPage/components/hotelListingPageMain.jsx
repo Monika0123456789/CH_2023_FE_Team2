48890b524a6ebabf5d8c0ef5951ec562f700a0d5
Mohammad Roohullah <Mohammad.Roohullah@valtech.com>
2023-11-21 14:52:02 +0530

    Delete hotels directory

--- hotels/src/webPages/hotelListingPage/components/hotelListingPageMain.jsx
+++
@@ -1,7 +0,0 @@
-// main component of this webpage -- here we will render the components of this page
-
-export let HotelListingPageMain = () => {
-  return <>
-  <h1>hello from main component of listing page</h1>
-  </>
-}
