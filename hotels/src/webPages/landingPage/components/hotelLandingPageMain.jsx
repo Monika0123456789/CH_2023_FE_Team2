105eb2f03de1d25cb241bf24f521321b9d1a3aca
Monika <monikamonika@1379@gmail.com>
2023-11-21 18:08:56 +0530

    folder structure

---
+++ hotels/src/webPages/landingPage/components/hotelLandingPageMain.jsx
@@ -0,0 +1,8 @@
+// main component of this webpage -- here we will render the components of this page
+export let HotelLandingPageMain = () => {
+  return (
+    <div>
+      <h1>hello from hotel landing page</h1>
+    </div>
+  );
+};
