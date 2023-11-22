37870168a2b4a65b806847912a63f42313348c30
Jeevan SJ <Jeevan.SJ@valtech.com>
2023-11-22 10:55:34 +0530

    Delete POC-2/hotels directory

--- POC-2/hotels/src/webPages/hotelListingPage/components/hotelListingPageMain.jsx
+++
@@ -1,8 +0,0 @@
-// main component of this webpage -- here we will render the components of this page
-export let HotelListingPageMain = () => {
-  return (
-    <div>
-      <h1>hello from hotel listing page</h1>
-    </div>
-  );
-};
