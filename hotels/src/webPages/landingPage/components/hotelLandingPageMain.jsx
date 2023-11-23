bb5363f9df4a52be1bf71ec547846c9fa96c5ec1
Suhas Indrigi <suhaspoo24@gmail.com>
2023-11-23 10:42:05 +0530

    creating the routes for the react application and implementing navigation features.

--- hotels/src/webPages/landingPage/components/hotelLandingPageMain.jsx
+++ hotels/src/webPages/landingPage/components/hotelLandingPageMain.jsx
@@ -3,13 +3,13 @@
 import { Section2 } from "./section2.component";
 import  ParentComponent  from "./section3-parentcomp";
 import { Section4 } from "./section4";
-import { HeaderComp } from "../../header/header";
+
 import { SectionOneComp } from "./section1";
 
 export let HotelLandingPageMain = () => {
   return (
     <div>
-      <HeaderComp/>
+      
       <SectionOneComp/>
       <Section2/>
      < ParentComponent />
--- hotels/src/webPages/main.jsx
+++ hotels/src/webPages/main.jsx
@@ -2,18 +2,36 @@
 import "./common.css";
 import { HotelListingPageMain } from "./hotelListingPage/components/hotelListingPageMain";
 import { HotelLandingPageMain } from "./landingPage/components/hotelLandingPageMain";
-import { Footer } from "./footer/components/footer"
+import { Footer } from "./footer/components/footer";
+import { HeaderComp } from "./header/components/header";
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 export let Main = () => {
   return (
-    <div className="container">
-    {/* landing page of the application renders here */}
-    <HotelLandingPageMain/>
+    // <div className="container">
+      
+    // {/* landing page of the application renders here */}
+    // <HotelLandingPageMain/>
 
-    {/* listing page of the application renders here */}
-    <HotelListingPageMain/>
-       <Footer/>
+    // {/* listing page of the application renders here */}
+    // <HotelListingPageMain/>
+
+    // {/* footer of the application renders here */}
+    // <Footer/>
+    // </div>
+    <div>
+      <HeaderComp/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <HotelLandingPageMain/> } />
+          <Route path="/hotelListingPage" element={ < HotelListingPageMain /> } />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer/>
     </div>
   );
 };
