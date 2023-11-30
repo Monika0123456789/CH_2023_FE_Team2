3006988452928a42b9808611c5e4034db53ed34f
Suhas Indrigi <suhaspoo24@gmail.com>
2023-11-30 14:13:51 +0530

    Fixing the major bug in the footer

--- hotels/src/webPages/footer/components/footer.jsx
+++ hotels/src/webPages/footer/components/footer.jsx
@@ -13,29 +13,29 @@ import { FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { MdAdminPanelSettings } from "react-icons/md";
-import "../footer-styles/footer.css";
+import style from "../footer-styles/footer.module.css";
 import { useNavigate } from "react-router-dom";
 
 export let Footer = () => {
     let navigate = useNavigate();
-    return <div className="footer_container">
+    return <div className={style.footer_container}>
         {/*Footer -------------------------Start-------------------------------------*/}
         <footer>
 
             {/* div representing the text and set of lists */}
-            <div className="footersection">
+            <div className={style.footersection}>
                 {/* div representing the text in the left part of the footer */}
-                <div className="first-footer-div">
-                    <img className="footerlogo" src="assets/images/logo-light.svg" alt="logo" />
+                <div className={style.first_footer_div}>
+                    <img className={style.footerlogo} src="assets/images/logo-light.svg" alt="logo" />
                     <p>Departure defective arranging rapturous did we believe him all had departed</p>
                     <span style={{color:"rgb(212, 209, 209)", marginTop:"15px"}}><FaPhoneAlt style={{color: "rgb(212, 209, 209)", marginRight:"10px"}}/>+1234 568 963</span>
                     <span style={{color:"rgb(212, 209, 209)", marginTop:"10px"}}><FaEnvelope style={{color: "rgb(212, 209, 209)", marginRight:"10px"}}/>example@gmail.com</span>
-                    <span style={{color:"rgb(212, 209, 209)", marginTop:"10px"}}><MdAdminPanelSettings style={{color: "rgb(212, 209, 209)", marginRight:"10px"}} className="admin_logo"/><a onClick={()=>{navigate('/adminlogin')}} style={{cursor:"pointer", textDecoration:"none"}}>Login as Admin</a></span>
+                    <span style={{color:"rgb(212, 209, 209)", marginTop:"10px"}}><MdAdminPanelSettings style={{color: "rgb(212, 209, 209)", marginRight:"10px"}} className={style.admin_logo}/><a onClick={()=>{navigate('/adminlogin')}} style={{cursor:"pointer", textDecoration:"none"}}>Login as Admin</a></span>
                 </div>
     
-                <div className="list-div">
+                <div className={style.list_div}>
                     {/* div representing the set of lists in the right */}
-                    <div className="list-footer">
+                    <div className={style.list_footer}>
                         <ul>
                             <h3>Page</h3>
                             <li><a href="#">About us</a></li>
@@ -44,7 +44,7 @@ export let Footer = () => {
                             <li><a href="#">Meet a Team</a></li>
                         </ul>
                     </div>
-                    <div className="list-footer">
+                    <div className={style.list_footer}>
                         <ul>
                             <h3>Link</h3>
                             <li><a href="#">Sign up</a></li>
@@ -55,7 +55,7 @@ export let Footer = () => {
                             <li><a href="#">Support</a></li>
                         </ul>
                     </div>
-                    <div className="list-footer">
+                    <div className={style.list_footer}>
                         <ul>
                             <h3>Global Site</h3>
                             <li><a href="#">India</a></li>
@@ -65,7 +65,7 @@ export let Footer = () => {
                             <li><a href="#">Malaysia</a></li>
                         </ul>
                     </div>
-                    <div className="list-footer">
+                    <div className={style.list_footer}>
                         <ul>
                             <h3>Booking</h3>
                             <li><FaHotel style={{color: "rgb(212, 209, 209)", marginRight:"10px"}}/><a href="#">Hotel</a></li>
@@ -78,48 +78,48 @@ export let Footer = () => {
     
             </div>
     
-            <h3 id="footer-top-links-h3">Top Links</h3>
-            <div className="footer-top-links-div">
-                <a className="footer-top-links-a" href="#">Flights</a>
-                <a className="footer-top-links-a" href="#">Hotels</a>
-                <a className="footer-top-links-a" href="#">Tours</a>
-                <a className="footer-top-links-a" href="#">Cabs</a>
-                <a className="footer-top-links-a" href="#">About</a>
-                <a className="footer-top-links-a" href="#">Contact us</a>
-                <a className="footer-top-links-a" href="#">Blogs</a>
-                <a className="footer-top-links-a" href="#">Services</a>
-                <a className="footer-top-links-a" href="#">Detail Page</a>
-                <a className="footer-top-links-a" href="#">Services</a>
-                <a className="footer-top-links-a" href="#">Policy</a>
-                <a className="footer-top-links-a" href="#">Testimonials</a>
-                <a className="footer-top-links-a" href="#">Newsletters</a>
-                <a className="footer-top-links-a" href="#">Podcasts</a>
-                <a className="footer-top-links-a" href="#">Protests</a>
-                <a className="footer-top-links-a" href="#">NewsCyber</a>
-                <a className="footer-top-links-a" href="#">Education</a>
-                <a className="footer-top-links-a" href="#">Sports</a>
-                <a className="footer-top-links-a" href="#">Tech and Auto</a>
-                <a className="footer-top-links-a" href="#">Opinion</a>
-                <a className="footer-top-links-a" href="#">Share Market</a>
+            <h3 id="footer_top_links_h3">Top Links</h3>
+            <div className={style.footer_top_links_div}>
+                <a className={style.footer_top_links_a} href="#">Flights</a>
+                <a className={style.footer_top_links_a} href="#">Hotels</a>
+                <a className={style.footer_top_links_a} href="#">Tours</a>
+                <a className={style.footer_top_links_a} href="#">Cabs</a>
+                <a className={style.footer_top_links_a} href="#">About</a>
+                <a className={style.footer_top_links_a} href="#">Contact us</a>
+                <a className={style.footer_top_links_a} href="#">Blogs</a>
+                <a className={style.footer_top_links_a} href="#">Services</a>
+                <a className={style.footer_top_links_a} href="#">Detail Page</a>
+                <a className={style.footer_top_links_a} href="#">Services</a>
+                <a className={style.footer_top_links_a} href="#">Policy</a>
+                <a className={style.footer_top_links_a} href="#">Testimonials</a>
+                <a className={style.footer_top_links_a} href="#">Newsletters</a>
+                <a className={style.footer_top_links_a} href="#">Podcasts</a>
+                <a className={style.footer_top_links_a} href="#">Protests</a>
+                <a className={style.footer_top_links_a} href="#">NewsCyber</a>
+                <a className={style.footer_top_links_a} href="#">Education</a>
+                <a className={style.footer_top_links_a} href="#">Sports</a>
+                <a className={style.footer_top_links_a} href="#">Tech and Auto</a>
+                <a className={style.footer_top_links_a} href="#">Opinion</a>
+                <a className={style.footer_top_links_a} href="#">Share Market</a>
             </div>
     
             {/* div representing the bottom most part of the footer */}
-            <div className="footer-bottom-div">
+            <div className={style.footer_bottom_div}>
                 {/* div representing the payment and security */}
-                <div className="footer-payment-and-security-div">
-                    <h3 id="footer-payment-and-security-h3">Payment and Security</h3>
-                    <div className="ps-icons">
+                <div className={style.footer_payment_and_security_div}>
+                    <h3 id={style.footer_payment_and_security_h3}>Payment and Security</h3>
+                    <div className={style.ps_icons}>
                         <a href="#"><FaCcPaypal style={{color: "#f0eaea"}}/></a>
-                        <a href="#"><FaCcVisa /><i className="fa-brands fa-cc-visa fa-2xl" style={{ color: "#f0eaea" }}></i></a>
+                        <a href="#"><FaCcVisa style={{color: "#f0eaea"}}/></a>
                         <a href="#"><FaCcMastercard style={{ color: "#f0eaea" }}/></a>
                         <a href="#"><SiAmericanexpress style={{ color: "#f0eaea" }}/></a>
                     </div>
                 </div>
     
                 {/* div representing the follow us */}
-                <div className="footer-follow-us-div">
-                    <h3 id="footer-follow-us-h3">Follow us on</h3>
-                    <div className="fu-icons">
+                <div className={style.footer_follow_us_div}>
+                    <h3 id={style.footer_follow_us_h3}>Follow us on</h3>
+                    <div className={style.fu_icons}>
                         <a href="#"><FaFacebook style={{ color: "#f0eaea" }}/></a>
                         <a href="#"><FaInstagram style={{ color: "#f0eaea" }}/></a>
                         <a href="#"><FaTwitter style={{ color: "#f0eaea" }}/></a>
@@ -131,7 +131,7 @@ export let Footer = () => {
             <hr/>
     
             {/* copyright text goes here */}
-            <h3 id="footer-copyright-text">Copyright &copy; 2023 Booking. All rights reserved</h3>
+            <h3 id={style.footer_copyright_text}>Copyright &copy; 2023 Booking. All rights reserved</h3>
         </footer>
     </div>
 }
