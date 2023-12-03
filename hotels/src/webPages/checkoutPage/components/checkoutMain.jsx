2232909742d07da39d6cdca60641a684e406b399
Mohammad Rouhulla <mrouhulla@gmail.com>
2023-12-03 20:46:57 +0530

    added the number of travellers field in local storage, updated validations for required fields in checkout form

--- hotels/src/webPages/checkoutPage/components/checkoutMain.jsx
+++ hotels/src/webPages/checkoutPage/components/checkoutMain.jsx
@@ -3,34 +3,57 @@ import { RoomComponent } from "./roomComponent";
 import { UserCredentialsComponent } from "./userCredentialsComponent";
 import style from "../checkout-page-styles/checkoutMain.module.css";
 import dataStore from "../store";
-
+import { SummaryComponent } from "./summaryComponent";
+import { Footer } from "../../footer/components/footer";
 
 export let CheckoutMain = () => {
   let storeData = () => {
+    const isUserDataValid = validateUserData(["address"]);
+
+    if (!isUserDataValid) {
+      alert("You must fill in all required fields .");
+      return;
+    }
     const data = dataStore.getState();
 
     const existingDataString = localStorage.getItem("Booking Details");
 
     const existingData = existingDataString
       ? JSON.parse(existingDataString)
-      : {bookingData : []};
+      : { bookingData: [] };
 
-       const bookingId = Date.now();
-          const newBooking = {
-            id : bookingId,
-            data,
-          };
+    const bookingId = Date.now();
+    const newBooking = {
+      id: bookingId,
+      data,
+    };
 
     const mergedData = {
       ...existingData,
-      bookingData:[
-        ...existingData.bookingData,
-        newBooking
-      ],
+      bookingData: [...existingData.bookingData, newBooking],
     };
 
     localStorage.setItem("Booking Details", JSON.stringify(mergedData));
     // console.log("Data saved to local storage:", data);
+  };
+  const validateUserData = (excludeFields = []) => {
+    const requiredFields = [
+      "fullName",
+      "email",
+      "mobileNumber",
+      "dateOfBirth",
+      "gender",
+      "checkin",
+      "checkout"
+    ];
+    const userData = dataStore.getState();
+    for (const field of requiredFields) {
+      if (!excludeFields.includes(field) && !userData[field]) {
+        return false;
+      }
+    }
+
+    return true;
   };
   return (
     <div style={{ width: "100%" }}>
@@ -42,10 +65,12 @@ export let CheckoutMain = () => {
       <br></br>
       <CheckInCheckOut />
       <br></br>
+      <SummaryComponent />
       <button className={style.submitBtn} onClick={storeData}>
         Proceed
       </button>
       <br></br>
+      <Footer/>
     </div>
   );
 };
--- hotels/src/webPages/checkoutPage/components/travellerDetailComponent.jsx
+++ hotels/src/webPages/checkoutPage/components/travellerDetailComponent.jsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
 import { Traveller } from "./traveller";
 import styles from "../checkout-page-styles/travellerDetailComponent.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decreaseTravellerCount,
+  increaseTravellerCount,
+} from "../redux/actions/roomQuantity.actions";
 
 export let TravellerComponent = () => {
-
   let [travellers, setTravellers] = useState([]);
+
+  const dispatch = useDispatch();
+  const travellersCount = useSelector((store) => store.numberOfTravellers);
+  console.log(travellersCount);
 
   // const addTravellerClick = () => {
   //   setTraveller(() => [...traveller, <Traveller idn={Math.random()}/>]);
   // };
-    const addTravellerClick = () => {
-      const newTraveller = {
-        // Use a unique ID (you can use a library like uuid for better uniqueness)
-        id: Date.now(),
-        component: <Traveller idn={Date.now()} onRemove={removeTraveller} />,
-      };
-
-      setTravellers((prevTravellers) => [...prevTravellers, newTraveller]);
+  const addTravellerClick = () => {
+    const newTraveller = {
+      // Use a unique ID (you can use a library like uuid for better uniqueness)
+      id: Date.now(),
+      component: <Traveller idn={Date.now()} onRemove={removeTraveller} />,
     };
-      const removeTraveller = (idToRemove) => {
-        setTravellers((prevTravellers) =>
-          prevTravellers.filter((traveller) => traveller.id !== idToRemove)
-        );
-      };
+
+    setTravellers((prevTravellers) => [...prevTravellers, newTraveller]);
+    dispatch(increaseTravellerCount());
+  };
+  const removeTraveller = (idToRemove) => {
+    setTravellers((prevTravellers) =>
+      prevTravellers.filter((traveller) => traveller.id !== idToRemove)
+    );
+    dispatch(decreaseTravellerCount());
+  };
   return (
     <div className={styles.travellerDetailContainer}>
       <h1>Travelers Detail</h1>
@@ -37,5 +47,3 @@ export let TravellerComponent = () => {
     </div>
   );
 };
-
-
