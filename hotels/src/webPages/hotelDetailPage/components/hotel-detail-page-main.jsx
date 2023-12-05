2438f8f5ba624a84acc78fa2c8d4fdf2accf76cb
Jeevan <Jeevan15sj@gmail.com>
2023-12-05 15:00:56 +0530

    Final push from myside with some additional css contents and changing the location of the json from public to src.

--- hotels/src/webPages/hotelDetailPage/components/hotel-detail-page-main.jsx
+++ hotels/src/webPages/hotelDetailPage/components/hotel-detail-page-main.jsx
@@ -4,7 +4,7 @@ import { Footer } from "../../footer/components/footer";
 import { DetailSectionOne } from "./section1";
 import { DetailSectionTwo } from "./section2";
 import { DetailSectionThree } from "./section3";
-import hotelData from '../../json/hotel-booking.json';
+import hotelData from '../../../json/hotel-booking.json';
 import commonStyle from "../../common.module.css"
 import { DetailSectionFive } from "./section5";
 import { SectionFour } from "./sectionfour";
