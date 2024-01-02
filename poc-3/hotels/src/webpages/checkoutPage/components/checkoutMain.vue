e1b7573b4a3e4e786803f34ed61a48bf1085849e
Suhas Indrigi <suhaspoo24@gmail.com>
2024-01-02 10:42:33 +0530

    checkin checkout, add new traveller details dynamically and booking summary component implemented.

--- poc-3/hotels/src/webpages/checkoutPage/components/checkoutMain.vue
+++ poc-3/hotels/src/webpages/checkoutPage/components/checkoutMain.vue
@@ -5,20 +5,37 @@
         <roomDetailComp/>
         <br>
         <userCredentialsComp/>
+        <br>
+        <checkinCheckoutComp/>
+        <br>
+        <travellerDetailMainComp/>
+        <br><br><br>
+        <summaryComp/>
+        <br><br>
     </div>
     
 </template>
 <script>
     import roomDetailComp from "./roomDetail.component.vue";
     import userCredentialsComp from "./userCredentials.component.vue";
+    import checkinCheckoutComp from "./checkinCheckout.component.vue";
+    import travellerDetailMainComp from "./travellerDetailMain.component.vue";
+    import summaryComp from "./summary.component.vue";
+
+    import { store } from '../../../store.js';
     export default{
         data(){
-
+            return{
+                store
+            }
         },
         components : {
             roomDetailComp,
-            userCredentialsComp
-        }
+            userCredentialsComp,
+            checkinCheckoutComp,
+            travellerDetailMainComp,
+            summaryComp
+        },
     }
 </script>
 <style></style>
