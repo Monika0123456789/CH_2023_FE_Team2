e248f543e69653f1a8f3f32df055fe2b4e7b2ad4
Mohammad Rouhulla <mrouhulla@gmail.com>
2024-01-11 15:09:32 +0530

    added section 2 of home screen of the app

--- poc-4/hotels/screens/homePage/components/homeScreenMain.jsx
+++ poc-4/hotels/screens/homePage/components/homeScreenMain.jsx
@@ -1,1 +1,13 @@
 // home screen main content goes here
+
+import { Text, View } from "react-native";
+import Section2 from "./section2.jsx";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      {/* <Text>home Screen</Text> */}
+      <Section2/>
+    </View>
+  );
+};
