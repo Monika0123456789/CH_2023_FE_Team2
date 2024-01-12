98fb42252eec5abfecb25e72b23c97bd079a2b82
Suhas Indrigi <suhaspoo24@gmail.com>
2024-01-12 18:10:05 +0530

    dynamically rendering the hotels in hotel listing page and changes in json file implemented.

--- poc-4/hotels/screens/hotelListingScreen/components/hotelListingScreenMain.jsx
+++ poc-4/hotels/screens/hotelListingScreen/components/hotelListingScreenMain.jsx
@@ -1,1 +1,17 @@
-// hotel listing main content goes here
+import { ScrollView, Text } from "react-native"
+import HotelListingScreenCard from "./hotelListingScreenCard";
+import data from "../../../assets/json/hotel-booking.json"
+
+export default HotelListingMain = () => {
+    return(
+        <>
+            <ScrollView>
+                {data.items.map((hotelListingObj, index) => (
+                    <HotelListingScreenCard key={index} hotelListingObj={hotelListingObj}/>
+                ))}
+            </ScrollView>
+            
+            
+        </>
+    )
+}
--- poc-4/hotels/screens/main.jsx
+++ poc-4/hotels/screens/main.jsx
@@ -1,11 +1,26 @@
 
-import  HomeScreenMain  from "./homePage/components/homeScreenMain"
+import  HomeScreenMain  from "./homePage/components/homeScreenMain";
+import HotelListingScreenMain from "./hotelListingScreen/components/hotelListingScreenMain";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export let Main = () => {
     return(
         <>
-            <HomeScreenMain/>
+            {/* <HomeScreenMain/> */}
+            <HotelListingScreenMain/>
         </>
+        
+        
+        // <NavigationContainer>
+        //     <Stack.Navigator>
+        //         <Stack.Screen name="Home" component={HomeScreenMain}/>
+        //         <Stack.Screen name="HotelListing" component={HotelListingScreenMain}/>
+        //     </Stack.Navigator>
+        // </NavigationContainer>
+
     )
 }
 
