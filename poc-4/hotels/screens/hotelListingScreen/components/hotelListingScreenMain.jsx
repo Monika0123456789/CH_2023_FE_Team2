80857ff174ce017106bfdec753d717dd489c71be
Monika <monikamonika@1379@gmail.com>
2024-01-24 14:37:02 +0530

    comment

--- poc-4/hotels/screens/hotelListingScreen/components/hotelListingScreenMain.jsx
+++ poc-4/hotels/screens/hotelListingScreen/components/hotelListingScreenMain.jsx
@@ -128,7 +128,7 @@
             { key: 'Amenities', text: 'Amenities' },
             { key: 'ClearAll', text: 'Clear All' },
           ];
-        // render the modal content based on the selected category
+        // render the modal content based on the selected categorysss
         const renderPopup = () => {
           switch (modalVisible) {
             case 'HotelTypes':
